04f95b599fee0ad570a7c7d803cb621217c581da
takumi-0505 <81544448+takumi-0505@users.noreply.github.com>
2021-04-14 18:33:47 +0900

    課題提出
    Task1_1.java、Task1_1.classの作成

---
+++ Java Study/1-1/Task1_1.java
@@ -0,0 +1,15 @@
+public class Task1_1 {
+
+    public static void main(String[] args) {
+        int hogeInt = 50;
+        String hogeString = "テスト";
+        //[double型を宣言して、変数名をhogeDoubleで、0.2で初期化]
+        double hogeDouble = 0.2;
+        hogeInt = 100;
+        hogeString = "合格";
+        System.out.println(hogeInt);
+        //[変数 hogeString の値を表示]
+        System.out.println(hogeString);
+        System.out.println(hogeDouble);
+    }
+}
